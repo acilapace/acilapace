6c214052a5e81c02fb4d089752108d66d2c69883
Mouslim Fatnassi <mouslim.fatnassi@opencontent.it>
2022-02-20 14:14:54 +0100

    fixed logo size, centered nav items and fixed widget based on mobile/desktop

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,34 +1,39 @@
 <template>
     <div id="navbar">
-        <b-navbar toggleable="lg" type="dark">
-            <b-navbar-brand @click="goToPage('index')"><img id="logo" src="../assets/logo_asslapace.png" alt="logo_asslapace">&nbsp; ASSOCIAZIONE LA PACE</b-navbar-brand>
-
+        <b-navbar toggleable="lg" type="dark">       
+            <div class="col-lg-4">
+                <b-navbar-brand @click="goToPage('index')"><img id="logo" src="../assets/logo_asslapace.png" alt="logo_asslapace">&nbsp; ASSOCIAZIONE LA PACE</b-navbar-brand>
+            </div>
             <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
-            <b-collapse id="nav-collapse" is-nav>
-                <b-navbar-nav>
-                    <b-nav-item @click="goToPage('index')">HOME</b-nav-item>
-                    <b-nav-item-dropdown text="ATTIVITA'" left>
-                        <b-dropdown-item @click="goToPage('Scuola')">Scuola di Lingua Araba ed Educazione Islamica</b-dropdown-item>
-                        <b-dropdown-item @click="goToPage('Integrazione')">Integrazione</b-dropdown-item>
-                        <b-dropdown-item @click="goToPage('SupportoES')">Supporto Economico-sociale</b-dropdown-item>
-                    </b-nav-item-dropdown>
-                     <b-nav-item-dropdown text="ISLAM" left>
-                        <b-dropdown-item @click="goToPage('DiscorsiVenerdi')">Discorsi del Venerdì</b-dropdown-item>
-                    </b-nav-item-dropdown>
-                    <b-nav-item @click="goToPage('Chi_Siamo')">CHI SIAMO</b-nav-item>
-                    <b-nav-item @click="goToPage('Contatti')">CONTATTACI</b-nav-item>
-                </b-navbar-nav>
-
-                <!-- Right aligned nav items -->
-                <b-navbar-nav class="ml-auto">
-                    <b-nav-item-dropdown text="LANG" right>
-                        <b-dropdown-item href="#">IT</b-dropdown-item>
-                        <b-dropdown-item href="#">EN</b-dropdown-item>
-                        <b-dropdown-item href="#">FR</b-dropdown-item>
-                    </b-nav-item-dropdown>
-                </b-navbar-nav>
-            </b-collapse>
+            <div class="col-lg-6">
+                <b-collapse id="nav-collapse" is-nav>
+                    <b-navbar-nav>
+                        <b-nav-item @click="goToPage('index')">HOME</b-nav-item>
+                        <b-nav-item-dropdown text="ATTIVITA'" left>
+                            <b-dropdown-item @click="goToPage('Scuola')">Scuola di Lingua Araba ed Educazione Islamica</b-dropdown-item>
+                            <b-dropdown-item @click="goToPage('Integrazione')">Integrazione</b-dropdown-item>
+                            <b-dropdown-item @click="goToPage('SupportoES')">Supporto Economico-sociale</b-dropdown-item>
+                        </b-nav-item-dropdown>
+                        <b-nav-item-dropdown text="ISLAM" left>
+                            <b-dropdown-item @click="goToPage('DiscorsiVenerdi')">Discorsi del Venerdì</b-dropdown-item>
+                        </b-nav-item-dropdown>
+                        <b-nav-item @click="goToPage('Chi_Siamo')">CHI SIAMO</b-nav-item>
+                        <b-nav-item @click="goToPage('Contatti')">CONTATTACI</b-nav-item>
+                    </b-navbar-nav>
+                </b-collapse>
+            </div>
+            <div class="col-lg-2">
+                <b-collapse id="nav-collapse" is-nav>
+                    <!-- Right aligned nav items -->
+                    <b-navbar-nav class="ml-auto">
+                        <b-nav-item-dropdown text="LANG" right>
+                            <b-dropdown-item href="#">IT</b-dropdown-item>
+                            <b-dropdown-item href="#">EN</b-dropdown-item>
+                            <b-dropdown-item href="#">FR</b-dropdown-item>
+                        </b-nav-item-dropdown>
+                    </b-navbar-nav>
+                </b-collapse>
+            </div>
         </b-navbar>
     </div>
 </template>
@@ -57,7 +62,7 @@ export default {
     }
 
     #logo {
-        width: 10%;
-        height: 10%;
+        width: 15%;
+        height: 15%;
     }
 </style>
